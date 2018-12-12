df125c71ade21a915600ab5e5de3591870a0ae34
Mike Griffin <github@mikegriffin.ie>
2018-12-12 22:00:46 +0000

    Extracts haml configuration

--- mpd-remote.rb
+++ mpd-remote.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/flash'
 require 'haml'
 
+set :haml, :format => :html5, :layout => :layout
+
 get '/' do
-	haml :index, format: :html5, layout: :layout
+	haml :index
 end
